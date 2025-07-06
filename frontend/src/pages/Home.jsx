1469afed53f779c4b5d882535b382c92bbf15cea
Aditya Kumar <aditya.95080@gmail.com>
2025-07-07 00:58:33 +0530

    Fix: correct Hero.jsx import case

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import hero from '../assets/hero.jpg';
-import Hero from '../components/hero';
+import Hero from '../components/Hero';
 function Home() {
   return (
     <>
